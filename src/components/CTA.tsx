52f27e16e2bc1422f637bfb219462f0d23bc7b7e
ialemautomacao <ialemautomacao@gmail.com>
2026-05-08 14:41:39 -0400

    Update CTA.tsx

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -28,7 +28,7 @@ export default function CTA() {
               <a href="https://wa.me/5500000000000?text=Oi!%20Vim%20pelo%20site%20e%20quero%20fazer%20um%20teste%20da%20IAlem%20na%20prática" target="_blank" className="btn btn-primary">
                 Testar na prática agora
               </a>
-              <a href="https://wa.me/5500000000000?text=Oi!%20Quero%20agendar%20um%20diagnóstico%20gratuito%20para%20minha%20empresa" target="_blank" className="btn btn-ghost">
+              <a href="https://wa.me/556992074081?text=Oi!%20Quero%20agendar%20um%20diagnóstico%20gratuito%20para%20minha%20empresa" target="_blank" className="btn btn-ghost">
                 Agendar diagnóstico
               </a>
             </div>
